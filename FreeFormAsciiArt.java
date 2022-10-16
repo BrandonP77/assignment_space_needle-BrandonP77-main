3402e36eb5acafca6b52198fb8fd2f762d0cc29c
BrandonP77 <bphilipp46@gmail.com>
2022-10-15 22:56:26 -0700

    Made progress on original art piece

--- FreeFormAsciiArt.java
+++ FreeFormAsciiArt.java
@@ -1,5 +1,12 @@
 public class FreeFormAsciiArt {
     public static void main(String[] args){
-        
+        System.out.println("One-Half");
+        System.out.println("--------+");
+        for(int i = 1; i <= 7; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
