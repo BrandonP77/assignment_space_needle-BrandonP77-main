0f7edc44428d9c0786f1f1a20fd1a2d225df00e0
BrandonP77 <bphilipp46@gmail.com>
2022-10-15 23:48:16 -0700

    Removed opening print statements

--- FreeFormAsciiArt.java
+++ FreeFormAsciiArt.java
@@ -1,7 +1,5 @@
 public class FreeFormAsciiArt {
     public static void main(String[] args){
-        System.out.println("One-Half");
-        System.out.println("--------+");
         for(int i = 1; i <= 7; i++){
             System.out.print("           |");
             for(int j = 1; j <= i; j++){
