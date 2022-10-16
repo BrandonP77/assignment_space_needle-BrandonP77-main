b61e3a52d73db0809682ed35654a8459d91682f4
BrandonP77 <bphilipp46@gmail.com>
2022-10-15 23:01:20 -0700

    Shifted the art piece to the right

--- FreeFormAsciiArt.java
+++ FreeFormAsciiArt.java
@@ -3,6 +3,7 @@ public class FreeFormAsciiArt {
         System.out.println("One-Half");
         System.out.println("--------+");
         for(int i = 1; i <= 7; i++){
+            System.out.print("           |");
             for(int j = 1; j <= i; j++){
                 System.out.print("*");
             }
