078b13f1aba8bfeecf19e0e5455f4b5bd27eec6e
BrandonP77 <bphilipp46@gmail.com>
2022-10-16 19:28:43 -0700

    Finished top-half of space needle platform

--- AssignedAsciiArt.java
+++ AssignedAsciiArt.java
@@ -1,11 +1,12 @@
 public class AssignedAsciiArt {
     public static void main(String[] args){
         for(int i = 1; i <= 3; i++){
-            System.out.println("         ||");
+            System.out.println("               ||");
         }
+        System.out.println("            __/||\\__");
         for(int j = 1; j <= 3; j++){
             // come back to this loop when you get back
-            for(int k = 3; k <= j; k++){
+            for(int k = 3; k >= j; k--){
                 System.out.print("   ");
             }
             System.out.print("__/");
@@ -13,7 +14,12 @@ public class AssignedAsciiArt {
                 System.out.print(":::");
             }
             System.out.print("||");
+            for(int l = 1; l <= j; l++){
+                System.out.print(":::");
+            }
+            System.out.print("\\__");
             System.out.println();
         }
+        System.out.println("   |\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|");
     }
 }
